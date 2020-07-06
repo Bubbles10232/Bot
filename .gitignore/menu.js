9e03366baa8ce043b2c8fe73bcd0b6ec9bce909b
Bubbles10232 <57570896+Bubbles10232@users.noreply.github.com>
2020-07-06 10:13:40 +0200

    Create menu.js

---
+++ .gitignore/menu.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("process.env.TOKEN")
+
+var prefix = ("vbl");
+
+client.on("message", (message) => {
+
+    if(message.content === "bonjour") {
+        message.channel.send("Salutation!")
+    }
+
+    if(message.content === prefix + "help"){
+       message.channel.send("Veux-tu de l'aide?")
+    }
+
+    
+});
